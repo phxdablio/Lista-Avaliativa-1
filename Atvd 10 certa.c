778044fb1a840975e919d49b69ff5d9100d4a244
Pedro Marques <116228124+phxdablio@users.noreply.github.com>
2023-05-05 15:50:43 -0300

    Add files via upload

---
+++ Atvd 10 certa.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main (){
+	
+	int n1,n2,n3,n4,n5,n6,n7,n8,n9,n10,soma,media;
+	
+	printf ("Digite uum numero para 1: ");
+	scanf ("%d",&n1);
+
+	printf ("Digite uum numero para 2: ");
+	scanf ("%d",&n2);
+
+	printf ("Digite uum numero para 3: ");
+	scanf ("%d",&n3);
+
+	printf ("Digite uum numero para 4: ");
+	scanf ("%d",&n4);
+
+	printf ("Digite uum numero para 5: ");
+	scanf ("%d",&n5);
+
+	printf ("Digite uum numero para 6: ");
+	scanf ("%d",&n6);
+
+	printf ("Digite uum numero para 7: ");
+	scanf ("%d",&n7);
+
+	printf ("Digite uum numero para 8: ");
+	scanf ("%d",&n8);
+
+	printf ("Digite uum numero para 9: ");
+	scanf ("%d",&n9);
+	
+	printf ("Digite uum numero para 10: ");
+	scanf ("%d",&n10);
+	
+	soma = n1+n2+n3+n4+n5+n6+n7+n8+n9+n10;
+	media = soma/10;
+	
+	printf ("A media eh: %d ");
+	
+}
